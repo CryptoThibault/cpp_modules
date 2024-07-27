3602ccb03fa9ac87d17f4773d3f24f3cb7a4b137
CryptoThibault <thibault.chalaoux@gmail.com>
2024-07-27 05:16:10 +0200

    cpp01

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int	main(void) {
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+	
+	std::cout << "String address: " << &string << std::endl;
+	std::cout << "StringPTR address: " << stringPTR << std::endl;
+	std::cout << "StringREF address: " << &stringREF << std::endl;
+	std::cout << "String value: " << string << std::endl;
+	std::cout << "StringPTR value: " << *stringPTR << std::endl;
+	std::cout << "StringREF value: " << stringREF << std::endl;
+	return 0;
+}
